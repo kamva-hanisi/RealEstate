9352da7c21ddc3872b4e56d741c785f1882cc3bf
kamva-hanisi <lucashanisi@gmail.com>
2025-04-15 17:59:46 +0200

    about helx

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -57,6 +57,66 @@ const About = () => {
           </div>
         </div>
       </section>
+
+      <section className="section">
+        <h2 className="head">What Drives Us</h2>
+        <div className="values">
+          <div className="card">
+            <h3>Integrity</h3>
+            <p>
+              We are committed to transparency and honesty in every transaction.
+            </p>
+          </div>
+          <div className="card">
+            <h3>Excellence</h3>
+            <p>We go above and beyond to provide top-tier service.</p>
+          </div>
+          <div className="card">
+            <h3>Innovation</h3>
+            <p>
+              Leveraging technology to enhance the property search experience.
+            </p>
+          </div>
+          <div className="card">
+            <h3>Customer-Centric</h3>
+            <p>Your needs are our top priority.</p>
+          </div>
+        </div>
+      </section>
+
+      <section className="section">
+        <h2 className="head">Meet Our Experts</h2>
+        <div className="team-grid">
+          <div className="team-member">
+            <img src="./public/about/person.jpg" alt="Rollins" />
+            <h3>Rollins Jali</h3>
+            <p>CEO</p>
+          </div>
+          <div className="team-member">
+            <img src="./public/about/khanyi.jpg" alt="Jennifer" />
+            <h3>Jennifer Sibanyoni</h3>
+            <p>Senior Agent</p>
+          </div>
+        </div>
+      </section>
+
+      <section className="section">
+        <h2 className="head">Why Homebuyers & Sellers Trust Us</h2>
+        <div className="hex-container">
+          <div>
+            <div className="hex">500+</div>
+            <div className="info">Happy Homeowners</div>
+          </div>
+          <div>
+            <div className="hex">10+</div>
+            <div className="info">Years of Industry Experience</div>
+          </div>
+          <div>
+            <div className="hex">100%</div>
+            <div className="info">Customer Satisfaction</div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
