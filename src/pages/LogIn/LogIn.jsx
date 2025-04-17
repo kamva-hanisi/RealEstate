fabc152cf3c81cd9e07c56e43e3b1448ff52cad8
kamva-hanisi <lucashanisi@gmail.com>
2025-04-17 15:38:11 +0200

    log in page

--- src/pages/LogIn/LogIn.jsx
+++ src/pages/LogIn/LogIn.jsx
@@ -1,7 +1,44 @@
 import React from "react";
+import "./LogIn.css";
+import { NavLink } from "react-router-dom";
 
 const LogIn = () => {
-  return <div>nvdkfjv</div>;
+  return (
+    <div className="su-container">
+      <h1>Log In</h1>
+
+      <div>
+        <form className="su-form">
+          <div className="input-group">
+            <label htmlFor="email">Email:</label>
+            <input
+              type="text"
+              id="email"
+              autoComplete="off"
+              placeholder="Type your Email"
+            />
+            <label htmlFor="password">Password:</label>
+            <input
+              type="text"
+              id="password"
+              autoComplete="off"
+              placeholder="Type your Password"
+            />
+
+            <p>
+              Forgot your password?{" "}
+              <NavLink to="/forgot-password" className="btn btn-link">
+                Reset Password
+              </NavLink>
+            </p>
+            <button type="submit" className="btn btn-outline-primary">
+              Log In
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default LogIn;
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -5,59 +5,61 @@ import { Button } from "bootstrap";
 
 const SignUp = () => {
   return (
-    <div className="su-container">
-      <h1>Sign Up</h1>
-      <p>Sign up to get started</p>
-      <div>
-        <form className="su-form">
-          <div className="input-group">
-            <label htmlFor="name">Name:</label>
-            <input
-              type="text"
-              id="name"
-              autoComplete="off"
-              placeholder="Enter your name"
-            />
-            <label htmlFor="email">Email:</label>
-            <input
-              type="text"
-              id="email"
-              autoComplete="off"
-              placeholder="Enter your Email"
-            />
-            <label htmlFor="password">Password:</label>
-            <input
-              type="text"
-              id="password"
-              autoComplete="off"
-              placeholder="Enter your Password"
-            />
-            <label htmlFor="confirm-password">Confirm Password:</label>
-            <input
-              type="text"
-              id="confirm-password"
-              autoComplete="off"
-              placeholder="Confirm your Password"
-            />
-            <checkbox className="checkbox">
-              <input type="checkbox" id="terms" name="terms" value="yes" />
-              Remember me
-            </checkbox>
-            <p>
-              By signing up, you agree to our
-              <a href="#">Terms of Service</a>
-            </p>
-            <button type="submit" className="btn btn-success">
-              Sign Up
-            </button>
+    <div className="su-bush">
+      <div className="su-container">
+        <h1>Sign Up</h1>
+        <p>Sign up to get started</p>
+        <div>
+          <form className="su-form">
+            <div className="input-group">
+              <label htmlFor="name">Name:</label>
+              <input
+                type="text"
+                id="name"
+                autoComplete="off"
+                placeholder="Enter your name"
+              />
+              <label htmlFor="email">Email:</label>
+              <input
+                type="text"
+                id="email"
+                autoComplete="off"
+                placeholder="Enter your Email"
+              />
+              <label htmlFor="password">Password:</label>
+              <input
+                type="text"
+                id="password"
+                autoComplete="off"
+                placeholder="Enter your Password"
+              />
+              <label htmlFor="confirm-password">Confirm Password:</label>
+              <input
+                type="text"
+                id="confirm-password"
+                autoComplete="off"
+                placeholder="Confirm your Password"
+              />
+              <checkbox className="checkbox">
+                <input type="checkbox" id="terms" name="terms" value="yes" />
+                Remember me
+              </checkbox>
+              <p>
+                By signing up, you agree to our
+                <a href="#">Terms of Service</a>
+              </p>
+              <button type="submit" className="btn btn-success">
+                Sign Up
+              </button>
+            </div>
+          </form>
+          <div className="login">
+            <p>Already have an account ?</p>
+            <NavLink to="/login" className="btn btn-primary">
+              <i className="fa-solid fa-arrow-right-to-bracket"></i>
+              Log In
+            </NavLink>
           </div>
-        </form>
-        <div className="login">
-          <p>Already have an account ?</p>
-          <NavLink to="/login" className="btn btn-primary">
-            <i className="fa-solid fa-arrow-right-to-bracket"></i>
-            Log In
-          </NavLink>
         </div>
       </div>
     </div>
